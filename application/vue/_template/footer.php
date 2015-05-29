8d8f9a32537aefb4d98400a8e69bf4de7c716986
thibaultfalque <thibault.falque@gmail.com>
2015-05-30 01:17:25 +0200

    correction Kint

--- application/vue/_template/footer.php
+++ application/vue/_template/footer.php
@@ -1,7 +1,5 @@
 <div class="footer">
-    <!-- echo out the content of the SESSION via KINT, a Composer-loaded much better version of var_dump -->
-    <!-- KINT can be used with the simple function d() -->
-    <?php +Kint::dump($_SESSION); ?>
+
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="public/js/bootstrap.min.js"></script>
