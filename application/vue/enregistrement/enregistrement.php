a320ca967a3ed754561f894f189a571e687a7828
xorgam <v.caffiaux@laposte.net>
2015-05-30 01:26:02 +0200

    formulaire enregistrement

--- application/vue/enregistrement/enregistrement.php
+++ application/vue/enregistrement/enregistrement.php
@@ -1,7 +1,10 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Vivien
- * Date: 30/05/2015
- * Time: 01:09
- */
+<section class="enregistrementform">
+    <form name="enregistrement" action="index_submit" method="get" accept-charset="utf-8">
+        Quel est votre type ?
+        <SELECT name="type" size="1">
+            <OPTION>Agent portuaire
+            <OPTION>Compagnie
+            <OPTION>Client
+        </SELECT>
+    </form>
+</section>
