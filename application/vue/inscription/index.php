75ab484a7826a2313eb9853eeeec20fb7f1101fb
xorgam <v.caffiaux@laposte.net>
2015-05-30 02:53:43 +0200

    formulaire enregistrement

--- application/vue/inscription/index.php
+++ application/vue/inscription/index.php
@@ -17,13 +17,13 @@
         <div id="type_0">
 
             <div class="form-group">
-                        <label for="email">Email : </label>
+                        <label for="email" class="col-sm-2 control-label">Email : </label>
                         <div class="col-sm-5">
                             <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required>
                         </div>
             </div>
             <div class="form-group">
-                        <label for="password">Mot de passe : </label>
+                        <label for="password" class="col-sm-2 control-label">Mot de passe : </label>
                         <div class="col-sm-5">
                             <input type="password" name="password" placeholder="Entrez le mot de passe ici" required>
                         </div>
@@ -33,28 +33,28 @@
         <div id="type_1" style="display:none;">
                 
             <div class="form-group">     
-                <label for="nom">Nom : </label>
+                <label for="nom" class="col-sm-2 control-label">Nom : </label>
                 <div class="col-sm-5">
                     <input type="nom" name="clientname" placeholder="etrez le nom ici" required>
                 </div>
             </div>
 
             <div class="form-group">
-                <label for="pays">Pays : </label>
+                <label for="pays" class="col-sm-2 control-label">Pays : </label>
                 <div class="col-sm-5">
                     <input type="pays" name="pays" placeholder="Entrez le pays ici" required>
                 </div>
             </div>
 
             <div class="form-group">
-                <label for="email">Email : </label>
+                <label for="email" class="col-sm-2 control-label">Email : </label>
                 <div class="col-sm-5">
                     <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required>
                 </div>
             </div>
 
             <div class="form-group">            
-                <label for="password">Mot de passe : </label>
+                <label for="password" class="col-sm-2 control-label">Mot de passe : </label>
                 <div class="col-sm-5">
                     <input type="password" name="password" placeholder="Entrez le mot de passe ici" required>
                 </div>
@@ -64,35 +64,35 @@
         <div id="type_2" style="display:none;">
 
             <div class="form-group">      
-                <label for="nom">Nom : </label>
+                <label for="nom" class="col-sm-2 control-label">Nom : </label>
                 <div class="col-sm-5">
                     <input type="nom" name="clientname" placeholder="etrez le nom ici" required>
                 </div>
             </div>
 
             <div class="form-group">        
-                <label for="adresse">Adresse : </label>
+                <label for="adresse" class="col-sm-2 control-label">Adresse : </label>
                 <div class="col-sm-5">
                     <input type="adresse" name="adresse" placeholder="entrez l'adresse ici" required>
                 </div>
             </div>
 
             <div class="form-group">
-                <label for="pays">Pays : </label>
+                <label for="pays" class="col-sm-2 control-label">Pays : </label>
                 <div class="col-sm-5">
                     <input type="pays" name="pays" placeholder="Entrez le pays ici" required>
                 </div>
             </div>
 
             <div class="form-group">
-                <label for="email">Email : </label>
+                <label for="email" class="col-sm-2 control-label">Email : </label>
                 <div class="col-sm-5">
                     <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required>
                 </div>
             </div>
 
             <div class="form-group">        
-                <label for="password">Mot de passe : </label>
+                <label for="password" class="col-sm-2 control-label">Mot de passe : </label>
                 <div class="col-sm-5">
                     <input type="password" name="password" placeholder="Entrez le mot de passe ici" required>
                 </div>
