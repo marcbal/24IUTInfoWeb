ffae8cc4e8b2e3086732030270bab40e80a9cf68
xorgam <v.caffiaux@laposte.net>
2015-05-30 02:51:33 +0200

    formulaire enregistrement

--- application/vue/inscription/index.php
+++ application/vue/inscription/index.php
@@ -5,12 +5,14 @@
 <section class="enregistrementform">
 
     <form name="enregistrement" action="index_submit" method="get" accept-charset="utf-8">
+        <div class="form-group">
         Quel est votre type ?
         <SELECT name="type" size="1" id="type_selector">
             <OPTION value="0">Agent portuaire</option>
             <OPTION value="1">Compagnie</option>
             <OPTION value="2">Client</option>
         </SELECT>
+        </div>
 
         <div id="type_0">
 
