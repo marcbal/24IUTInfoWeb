9be0ecd36167d1c11ceafc94a45d2ae59fcdfd2e
antoinebacquet <antoinebacquet1@gmail.com>
2015-05-30 05:14:32 +0200

    bug

--- application/modele/mymodel/tables/Conteneurs.class.php
+++ application/modele/mymodel/tables/Conteneurs.class.php
@@ -23,7 +23,7 @@ class Conteneurs extends Table{
 
     public function countMov(MouvementsSQL $queryMovement){
         $this->nbC = count($queryMovement->findWithCondition('id_conteneur = ? and type = c',array($this->id)));
-        $this->nbd = count($queryMovement->findWithCondition('id_conteneur = ? and type = d', array($this->id)));
+        $this->nbD = count($queryMovement->findWithCondition('id_conteneur = ? and type = d', array($this->id)));
     }
 
 }
