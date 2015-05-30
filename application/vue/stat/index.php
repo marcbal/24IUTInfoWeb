ea79918ae75c9628b834ac6ac017b836d8e59846
thibaultfalque <thibault.falque@gmail.com>
2015-05-30 02:57:47 +0200

    ajout navire et escale

--- application/vue/stat/index.php
+++ application/vue/stat/index.php
@@ -14,7 +14,8 @@
     <?php } ?>
 </select>
 <label for="annee">Année:</label><input id="annee" name="annee" type="num" />
-<select>
+<label for="mois">Mois:</label>
+<select name="mois">
     <option value="1">Janvier</option>
     <option value="2">Février</option>
     <option value="3">Mars</option>
@@ -28,3 +29,19 @@
     <option value="11">Novembre</option>
     <option value="12">Décembre</option>
 </select>
+<label for="navire">Navire:</label>
+<select name="navire">
+    <option value="tous">Tous les navires</option>
+    <?php foreach($navires as $navire){
+        ?>
+        <option value="<?php echo $navire->getId();?>" ><?php  echo $navire->nom ?></option>
+    <?php } ?>
+</select>
+<div style="display: none">
+    <select name="escale">
+        <?php foreach($escales as $escale){
+            ?>
+            <option value="<?php echo $escale->getId();?>" ><?php  echo $escale->date_entree." ".$escale->date_sortie ?></option>
+        <?php } ?>
+    </select>
+</div>
