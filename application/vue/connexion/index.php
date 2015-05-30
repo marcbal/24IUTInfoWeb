20a028d89ffd86d7b5f99f758376906135eb3e30
Babanar <louis.behague@orange.fr>
2015-05-30 02:43:44 +0200

    Connexion

--- application/vue/connexion/index.php
+++ application/vue/connexion/index.php
@@ -1,6 +1,7 @@
 <!-- Bootstrap core CSS -->
 <link href="<?php echo URL; ?>public/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?php echo URL; ?>public/css/navbar-fixed-top.css" rel="stylesheet">
+<?php Session::renderFeedback("login") ?>
 <section class="loginform">
 
 
