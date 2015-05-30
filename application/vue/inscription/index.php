3f58d927e3e7b65331b0b9e488ad16f66bec1b44
xorgam <v.caffiaux@laposte.net>
2015-05-30 02:39:51 +0200

    formulaire enregistrement

--- application/vue/inscription/index.php
+++ application/vue/inscription/index.php
@@ -4,41 +4,71 @@
         <SELECT name="type" size="1" id="type_selector">
             <OPTION value="0">Agent portuaire</option>
             <OPTION value="1">Compagnie</option>
-            <OPTION value="2">Client </option>
+            <OPTION value="2">Client</option>
         </SELECT>
-        <div id="type_0" >
-                    <li><label for="email">Email : </label>
-                        <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required></li>
-                    <li><label for="password">Mot de passe : </label>
-                        <input type="password" name="password" placeholder="Entrez le mot de passe ici" required></li>
+
+        <div id="type_0">
+
+            <div class="form-group">
+                        <label for="email">Email : </label>
+                        <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required>
+            </div>
+            <div class="form-group">
+                        <label for="password">Mot de passe : </label>
+                        <input type="password" name="password" placeholder="Entrez le mot de passe ici" required>
+            </div>
+
         </div>
         <div id="type_1" style="display:none;">
-                <ul>
-                        <li><label for="nom">Nom : </label>
-                            <input type="nom" name="clientname" placeholder="etrez le nom ici" required></li>
-                        <li><label for="pays">Pays : </label>
-                            <input type="pays" name="pays" placeholder="Entrez le pays ici" required></li>
-                        <li><label for="email">Email : </label>
-                            <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required></li>
-                        <li><label for="password">Mot de passe : </label>
-                            <input type="password" name="password" placeholder="Entrez le mot de passe ici" required></li>
-                </ul>
+                
+            <div class="form-group">     
+                <label for="nom">Nom : </label>
+                <input type="nom" name="clientname" placeholder="etrez le nom ici" required>
+            </div>
+
+            <div class="form-group">
+                <label for="pays">Pays : </label>
+                <input type="pays" name="pays" placeholder="Entrez le pays ici" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email : </label>
+                <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required>
+            </div>
+
+            <div class="form-group">            
+                <label for="password">Mot de passe : </label>
+                <input type="password" name="password" placeholder="Entrez le mot de passe ici" required>
+            </div>
 
         </div>
         <div id="type_2" style="display:none;">
 
-                <ul>
-                        <li><label for="nom">Nom : </label>
-                            <input type="nom" name="clientname" placeholder="etrez le nom ici" required></li>
-                        <li><label for="adresse">Adresse : </label>
-                            <input type="adresse" name="adresse" placeholder="entrez l'adresse ici" required></li>
-                        <li><label for="pays">Pays : </label>
-                            <input type="pays" name="pays" placeholder="Entrez le pays ici" required></li>
-                        <li><label for="email">Email : </label>
-                            <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required></li>
-                        <li><label for="password">Mot de passe : </label>
-                            <input type="password" name="password" placeholder="Entrez le mot de passe ici" required></li>
-                </ul>
+            <div class="form-group">      
+                <label for="nom">Nom : </label>
+                <input type="nom" name="clientname" placeholder="etrez le nom ici" required>
+            </div>
+
+            <div class="form-group">        
+                <label for="adresse">Adresse : </label>
+                <input type="adresse" name="adresse" placeholder="entrez l'adresse ici" required>
+            </div>
+
+            <div class="form-group">
+                <label for="pays">Pays : </label>
+                <input type="pays" name="pays" placeholder="Entrez le pays ici" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email : </label>
+                <input type="email" name="email" placeholder="Entrez l'adresse mail ici" required>
+            </div>
+
+            <div class="form-group">        
+                <label for="password">Mot de passe : </label>
+                <input type="password" name="password" placeholder="Entrez le mot de passe ici" required>
+            </div>
+
         </div>
     </form>
 </section>
