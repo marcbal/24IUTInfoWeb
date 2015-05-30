75dc4832ab1e49e8d72affd770d7a02fadf468d2
thibaultfalque <thibault.falque@gmail.com>
2015-05-30 04:10:44 +0200

    amélioration input

--- application/controleur/accueil.php
+++ application/controleur/accueil.php
@@ -14,7 +14,7 @@ class Accueil extends Controleur
             Redirect::login ();
     }
     
-    public function index()
+    public function index($args)
     {
         require 'application/vue/_template/header.php';
         require 'application/vue/accueil/index.php';
