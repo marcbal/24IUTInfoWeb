5a46b90c8eddb1a827108e8d674658416b0aa34a
antoinebacquet <antoinebacquet1@gmail.com>
2015-05-30 03:10:17 +0200

    bug

--- application/controleur/compagnie.php
+++ application/controleur/compagnie.php
@@ -17,6 +17,8 @@ class Compagnie extends Controleur
 
     public function index()
     {
+
+        parent::loadModel('Compagnies');
         $query = new CompagniesSQL();
         $compagnie = $query->findById(Session::get('id'));
 
@@ -27,6 +29,7 @@ class Compagnie extends Controleur
 
     public function navires(){
         parent::loadModel('Compagnies');
+        parent::loadModel('Navires');
 
         $query = new CompagniesSQL();
         $compagnie = $query->findById(Session::get('id'));
