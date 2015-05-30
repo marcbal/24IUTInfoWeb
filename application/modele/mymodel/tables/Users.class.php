a31d854c68ca4b3d861b8ce3f679b777859d90a8
zeroCrowsky <veis.oudjail@gmail.com>
2015-05-30 05:31:36 +0200

    dddddd

--- application/modele/mymodel/tables/Users.class.php
+++ application/modele/mymodel/tables/Users.class.php
@@ -9,6 +9,7 @@ class Users extends Table {
     public $id_compagnie;
     public $id_agent;
     function __construct($user_password_hash="", $user_email="", $user_type="", $id_client="", $id_compagnie="", $id_agent="") {
+        parent::__construct();
         $this->user_password_hash = $user_password_hash;
         $this->user_email = $user_email;
         $this->user_type = $user_type;
